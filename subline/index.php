dfc1a112b0fbba80e3aa377330144747705cedc2
Addressday <58471834+Addressday@users.noreply.github.com>
2020-11-19 15:09:18 +0900

    Add files via upload

---
+++ subline/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>구글클론사이트111</title>
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+</head>
+<body>
+
+		<!-- 웹페이지 화면을 감싸는 웹퍼 클래스. 인덱스 페이지 -->
+		<div class="wrapper indexPage">
+
+			<!-- 메인섹션 -->
+			<div class="mainSection">
+				
+				<!-- 로고부분 -->
+				<div class="logoContainer">
+					
+				</div>
+
+				<!-- 검색부분 -->
+				<div class="searchContainer">
+					
+					<input class="searchBox" type="text" placeholder="검색어를 입력해주세요...">
+
+				</div>
+			</div>
+		</div>
+</body>
+</html>
